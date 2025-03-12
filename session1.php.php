53ae3885e258a1201a327f9482a45c74eb0a1c0e
TomMaxil <bilalnadeem2626@gmail.com>
2025-03-12 21:26:33 +0500

    First File Php

---
+++ session1.php.php
@@ -0,0 +1,52 @@
+<?php
+    
+    // echo "Hello World";
+    // echo "<br>";
+
+    // echo "2+2";
+    // echo "<br>";
+
+    //echo  "Result Is " . 2+2;
+
+    /*Variables*/
+
+    // $batch = "PR2_202406E";
+    // //echo $batch;
+
+    // echo "<h1>".$batch."</h1>";
+    // echo "My Batch Code is " . $batch;
+    // echo "<hr>";
+
+    // $value1 = 50;
+    // $value2 = 7;
+
+    // $result = $value1 + $value2;
+    // echo "The Result is" . $result;
+
+    // echo "<hr>";
+
+    $name = "Bilal";
+    $age = 19;
+    $height = 5.2;
+    $isGraduated = false;
+    $sentence = "the quick brown fox jumps over the lazy dog";
+
+    /*Types*/
+    // echo gettype($name);
+    // echo "<hr>";
+    // echo gettype($age);
+    // echo "<hr>";
+    // echo gettype($height);
+    // echo "<hr>";
+    // echo gettype($isGraduated);
+    // echo "<hr>";
+    // echo gettype($sentence);
+    
+    /*Built in Function*/
+    //echo strlen($name); /*For Count Lenght*/
+    //echo str_word_count($name); /*For Count Word*/
+    //echo strrev($name) /*For Reverese Word*/
+    //echo strpos($name,"i"); /*For Count Word Lenght*/
+    //echo str_replace('B','A',$name); /*For Replace With Any Word*/
+    echo ucwords($sentence); /*For Convert Every Word Fist Letter Uppercase In Sentence*/
+?>
