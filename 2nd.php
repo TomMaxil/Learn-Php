addf16bca8b7b8db4cac7635614693efcdbe7ba0
TomMaxil <bilalnadeem2626@gmail.com>
2025-03-17 16:39:13 +0500

    Php 2nd File

---
+++ 2nd.php
@@ -0,0 +1,38 @@
+<?php
+
+echo "------Maths------<br>";
+
+//echo pi();
+
+$age =[20,25,60,19,50];
+
+//echo max($age);
+//echo min($age);
+
+//echo rand();
+//echo rand(100,500);
+
+define("name",'Bilal');
+//define("name",'Hamza');
+//echo name;
+
+//echo "line no :" . __LINE__;
+//echo __DIR__;
+//echo __FILE__;
+
+$std_name = "Bilal";
+$std_age = 19;
+$std_height = 5.4;
+$std_graudated =FALSE;
+$std_hobbies =null;
+
+//echo gettype($std_name);
+//echo is_int($std_age);
+//echo var_dump($std_height);
+//echo var_dump($std_graudated);
+echo var_dump($std_hobbies);
+
+
+
+
+?>
